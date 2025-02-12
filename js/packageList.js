2d66439ec4deb929623d77e315b9b19d2c1f3642
yashvarmora2306 <yashpatel23062002@gmail.com>
2025-02-12 17:23:21 +0530

    implement pagination for package list

--- js/packageList.js
+++ js/packageList.js
@@ -1,3 +1,6 @@
+const itemsPerPage = 6;
+let currentPage = 1;
+
 
 const removePackage = (name) => {
     let packages = JSON.parse(localStorage.getItem("packages")) || [];
@@ -9,9 +12,18 @@ const removePackage = (name) => {
 const loadPackages = () => {
     let packages = JSON.parse(localStorage.getItem("packages")) || [];
     const packageList = document.getElementById("packageList");
+    const pagination = document.getElementById("pagination");
     packageList.innerHTML = "";
+    pagination.innerHTML = "";
 
-    packages.forEach((pkg) => {
+    const totalPages = Math.ceil(packages.length / itemsPerPage);
+    if (currentPage > totalPages) currentPage = totalPages || 1;
+
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const paginatedItems = packages.slice(startIndex, startIndex + itemsPerPage);
+
+
+    paginatedItems.forEach((pkg) => {
         const packageDiv = document.createElement("div");
         packageDiv.classList.add("card");
         packageDiv.innerHTML = `
@@ -30,6 +42,44 @@ const loadPackages = () => {
         packageList.appendChild(packageDiv);
 
     });
+
+    if (totalPages > 1) {
+        const prevButton = document.createElement("button");
+        prevButton.innerText = "Prev";
+        prevButton.classList.add("page-btn");
+        prevButton.disabled = currentPage === 1
+        prevButton.addEventListener("click", () => {
+            if (currentPage > 1) {
+                currentPage--;
+                loadPackages();
+            }
+        });
+        pagination.appendChild(prevButton);
+
+        for (let i = 1; i <= totalPages; i++) {
+            const pageButton = document.createElement("button");
+            pageButton.innerText = i;
+            pageButton.classList.add("page-btn");
+            if (i === currentPage) pageButton.classList.add("active");
+            pageButton.addEventListener("click", () => {
+                currentPage = i;
+                loadPackages();
+            });
+            pagination.appendChild(pageButton);
+        }
+
+        const nextButton = document.createElement("button");
+        nextButton.innerText = "Next";
+        nextButton.classList.add("page-btn");
+        nextButton.disabled = currentPage === totalPages;
+        nextButton.addEventListener("click", () => {
+            if (currentPage < totalPages) {
+                currentPage++;
+                loadPackages();
+            }
+        });
+        pagination.appendChild(nextButton);
+    }
 }
 
 document.addEventListener("DOMContentLoaded", loadPackages);
